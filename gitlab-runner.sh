93065ec79dda24fdbe53752180b90d746adba59e
vmn3 <nichiporenko.vitaly@gmail.com>
2022-03-23 11:12:07 +0300

    Updted

--- gitlab-runner.sh
+++ gitlab-runner.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg-agent
-
-# install docker
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo usermod -a -G docker $USER
-sudo usermod -a -G docker ubuntu
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo apt install docker-compose -y
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
 
 # install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /tmp/docker-compose
@@ -26,6 +20,8 @@ mkdir -p /var/opt/wp
 mkdir -p /var/opt/wp/nginx
 cd /var/opt/wp
 
+
+
 echo "${dockercompose}" > docker-compose.yml
 echo "${nginx_conf}" > nginx/server.conf
 
@@ -33,6 +29,7 @@ export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
 
 docker-compose up
+sudo systemctl restart docker
 
 # Intstall gitlab-runner
 # Create a GitLab CI user
